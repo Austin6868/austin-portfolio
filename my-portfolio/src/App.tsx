23127d9a391786f0b4c367363ed9234ce79dc445
Austin6868 <zzha63@uwo.ca>
2022-10-04 19:25:55 -0400

    Import Tailwind CSS

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <span>This is my site</span>
+      <span className="text-lg text-red-800">This is my site</span>
     </div>
   );
 }
