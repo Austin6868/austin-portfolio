499d8737b2d399f7604bb408ad02c43b3967617d
Austin6868 <zzha63@uwo.ca>
2022-10-05 00:51:31 -0400

    added navbar

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App(){
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Homepage />}>
           <Route index element={<Homepage />} />
---
+++ my-portfolio/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Navbar(){
+
+    return(
+        <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+            This is the Nav Bar
+        </div>
+    );
+}
+
+export default Navbar;
