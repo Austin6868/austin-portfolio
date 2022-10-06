4d52a0c1ad8f1e1f8d2c472277ac7b3ae6db3db8
Austin6868 <zzha63@uwo.ca>
2022-10-06 02:10:00 -0400

    Added contact and about pages

---
+++ my-portfolio/src/pages/ContactMe.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Homepage(){
+
+    return(
+        <div className="antialiased h-screen text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900">This is the home page</div>
+    );
+}
+
+export default Homepage;
