6fb5b74faad2c53de1912935f436e7c286677239
Austin6868 <zzha63@uwo.ca>
2022-10-08 18:31:49 -0400

    implement menu items on navbar and build

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Link } from "react-router-dom";
+import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
 
 function Navbar(){
 
     return(
-        <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
-            This is the Nav Bar
+        <header className="absolute z-10 w-full h-20 flex items-center shadow-md nav-background">
+      <div className="w-full text-white flex justify-content-center pl-5 lg:pl-12 pr-5 lg:pr-12 items-center flex-row">
+        <div className="w-full flex flex-row justify-between">
+          <Link className="text-3xl font-semibold" to="/">
+            Austin Zhang
+          </Link>
+          <div className="flex items-center space-x-16">
+            <div className="hidden lg:block lg:ml-12 text-xl font-normal text-opacity-75">
+              <Menu menuButton={<MenuButton>Projects</MenuButton>} transition>
+                <span className="pl-1 font-bold">🚀 Finished</span>
+                <MenuItem>court-booking-app</MenuItem>
+                <MenuItem>instructions+</MenuItem>
+                <MenuItem>rpg-game-project</MenuItem>
+                <span className="pl-1 font-bold">🚧 Under Construction</span>
+                <MenuItem>votingbuck</MenuItem>
+                <MenuItem>wot-aggregator</MenuItem>
+              </Menu>
+            </div>
+            <div className="hidden lg:block lg:ml-12 text-xl font-normal text-opacity-75">
+              <Menu menuButton={<MenuButton>Internships</MenuButton>} transition>
+                <MenuItem>
+                  <Link to="/politicians">RBC Amplify</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/corporations">RBC TI</Link>
+                </MenuItem>
+              </Menu>
+            </div>
+            <div className="hidden lg:block lg:ml-12 text-xl font-normal text-opacity-75">
+              <Link to="/politicians">Contact me</Link>
+            </div>
+            <div className="hidden lg:block lg:ml-12 text-xl font-normal text-opacity-75">
+              <Link to="/about">About</Link>
+            </div>
+          </div>
         </div>
+      </div>
+    </header>
     );
 }
 
--- my-portfolio/src/pages/Homepage.tsx
+++ my-portfolio/src/pages/Homepage.tsx
@@ -1,56 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import Divider from "@mui/material/Divider";
-import Paper from "@mui/material/Paper";
-import MenuList from "@mui/material/MenuList";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Typography from "@mui/material/Typography";
-import ContentCut from "@mui/icons-material/ContentCut";
-import ContentCopy from "@mui/icons-material/ContentCopy";
-import ContentPaste from "@mui/icons-material/ContentPaste";
-import Cloud from "@mui/icons-material/Cloud";
-import { Css, Html, Javascript } from "@mui/icons-material";
 
 function Homepage(){
     return(
-        <div>
+        <div className="flex-col items-center">
         <div className="antialiased h-screen text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900">This is the home page</div>
-            <br />
-            <Paper sx={{ 
-                width: 320, 
-                maxWidth: "100%", 
-                margin: "auto" 
-            }}>
-                <MenuList>
-                    <MenuItem>
-                        <ListItemIcon>
-                            <Html fontSize="small" />
-                        </ListItemIcon>
-                        <ListItemText>HTML</ListItemText>
-                    </MenuItem>
-                    <MenuItem>
-                        <ListItemIcon>
-                            <Css fontSize="small" />
-                        </ListItemIcon>
-                        <ListItemText>CSS</ListItemText>
-                    </MenuItem>
-                    <MenuItem>
-                        <ListItemIcon>
-                            <Javascript fontSize="small" />
-                        </ListItemIcon>
-                        <ListItemText>Javascript</ListItemText>
-                    </MenuItem>
-                    <Divider />
-                    <MenuItem>
-                        <ListItemIcon>
-                            <Cloud fontSize="small" />
-                        </ListItemIcon>
-                        <ListItemText>Cloud </ListItemText>
-                    </MenuItem>
-                </MenuList>
-            </Paper>
         </div>
     );
 }
