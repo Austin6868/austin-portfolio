c5e2e095b3112d80a1db1094ee953dcad7e2eb5d
Austin6868 <zzha63@uwo.ca>
2022-10-09 22:41:36 -0400

    added card content

--- my-portfolio/src/components/DemoCard.tsx
+++ my-portfolio/src/components/DemoCard.tsx
@@ -4,35 +4,37 @@ import {
     CardBody,
     CardFooter,
     Typography,
-  } from "@material-tailwind/react";
-   
-  export default function Example() {
-    return (
-      <Card className="w-96">
-        <CardHeader color="blue" className="relative h-56">
-          <img
-            src="/img/blog.jpg"
-            alt="img-blur-shadow"
-            className="h-full w-full"
-          />
-        </CardHeader>
-        <CardBody className="text-center">
-          <Typography variant="h5" className="mb-2">
-            Cozy 5 Stars Apartment
-          </Typography>
-          <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to "Naviglio" where you can enjoy the main night life in
-            Barcelona.
-          </Typography>
-        </CardBody>
-        <CardFooter divider className="flex items-center justify-between py-3">
-          <Typography variant="small">$899/night</Typography>
-          <Typography variant="small" color="gray" className="flex gap-1">
-            <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-            Barcelona, Spain
-          </Typography>
-        </CardFooter>
-      </Card>
-    );
+} from "@material-tailwind/react";
+
+const techList = ["Django", "Bootstrap", "SQLite", "AWS"]
+
+function importAll(r: any) {
+
+    let images = {};
+    r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+    return images;
+
   }
+
+const images = importAll(require.context('../Assets/', false, /\.(png|jpe?g|svg)$/));
+console.log(images)
+
+export default function Example() {
+    
+    return (
+        <div className="rounded-xl bg-slate-900 shadow-xl w-[80vw] h-[70vh]">
+            <div className="card-body">
+                <span className="text-3xl">Court booking app</span>
+                <p>Tech Stack</p>
+                <div className="flex space-x-12">
+                {
+                     techList.map((item)=> <img className="h-10" src={images[item + ".svg"]}/>)
+                }   
+                </div>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-blue">Learn More</button>
+                </div>
+            </div>
+        </div>
+    );
+}
